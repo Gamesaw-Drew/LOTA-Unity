8dee8df759f859ba3a531349856c307ade9aa56f
Drew <drewgamesaw@gmail.com>
2017-04-17 18:24:20 -0400

    Introduce HR_CHRIS - a more advanced version of the model, not used at all yet - only the talk animation is made in it, and it needs work

---
+++ Assets/CloseCull.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseCull : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Camera camera = GetComponent<Camera>();
+		float[] distances = new float[32];
+		distances [8] = 110;
+		distances [9] = 140;
+		camera.layerCullDistances = distances;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
